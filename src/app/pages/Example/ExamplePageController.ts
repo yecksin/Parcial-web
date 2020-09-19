034342ee4ceb8f439f4ca5fdc4841ee0023a841f
Yecksin Mauricio <yecksin@gmail.com>
2020-09-19 01:04:04 -0500

    Agregar colores y sección presentación

--- src/app/pages/Example/ExamplePageController.ts
+++ src/app/pages/Example/ExamplePageController.ts
@@ -13,27 +13,27 @@ export class ExamplePageController {
     }
 
     start() {
-        const box: any = this.findInsideMe(".box");
-        const TDBox: any = this.findInsideMe(".TDBox");
-        const clouds: any = this.findInsideMe(".cloud", true);
-        window.onscroll = (_:any) => {
-            console.log(`X: ${window.scrollX} Y: ${window.scrollY}`)
-            const p = (window.scrollY / window.innerHeight) * 100;
-            box.style.left = `${p}%`;
-            box.style.opacity = `${p / 100}`;
+        // const box: any = this.findInsideMe(".box");
+        // const TDBox: any = this.findInsideMe(".TDBox");
+        // const clouds: any = this.findInsideMe(".cloud", true);
+        // window.onscroll = (_:any) => {
+        //     console.log(`X: ${window.scrollX} Y: ${window.scrollY}`)
+        //     const p = (window.scrollY / window.innerHeight) * 100;
+        //     box.style.left = `${p}%`;
+        //     box.style.opacity = `${p / 100}`;
 
-            clouds[0].style.left = `${p * 2}%`;
-            clouds[1].style.left = `${p * 1.3}%`;
+        //     clouds[0].style.left = `${p * 2}%`;
+        //     clouds[1].style.left = `${p * 1.3}%`;
             
-            if (window.scrollY >= 340) {
-                TDBox.style.opacity = 1;
-                if (window.scrollY >= 350) {
-                    TDBox.style.transform = `rotateY(${window.scrollY}deg)`;
-                }
-            } else {
-                TDBox.style.opacity = 0;
-            }
-        }
+        //     if (window.scrollY >= 340) {
+        //         TDBox.style.opacity = 1;
+        //         if (window.scrollY >= 350) {
+        //             TDBox.style.transform = `rotateY(${window.scrollY}deg)`;
+        //         }
+        //     } else {
+        //         TDBox.style.opacity = 0;
+        //     }
+        // }
     }
 
     findInsideMe(selector: string, all = false) {
