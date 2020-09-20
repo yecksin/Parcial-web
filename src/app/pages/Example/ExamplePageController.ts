50b19a2746e92392543fc35085679545dddebfe5
Yecksin Mauricio <yecksin@gmail.com>
2020-09-20 15:44:26 -0500

    Acomodamo sel titulo, colores, section altura

--- src/app/pages/Example/ExamplePageController.ts
+++ src/app/pages/Example/ExamplePageController.ts
@@ -14,45 +14,45 @@ export class ExamplePageController {
     }
 
     start() {
-        const checkpoint = 300;
+        // const checkpoint = 300;
 
-        let opacity = 0;
-        window.addEventListener("scroll", () => {
-            const box: any = this.findInsideMe(".box");
-            console.log(window.pageYOffset + " " + box.getBoundingClientRect().top);
-            const currentScroll = window.pageYOffset;
-            if (currentScroll <= checkpoint) {
-                opacity = 1 - currentScroll / checkpoint;
-            } else {
-                opacity = 0;
-            }
+        // let opacity = 0;
+        // window.addEventListener("scroll", () => {
+        //     const box: any = this.findInsideMe(".box");
+        //     console.log(window.pageYOffset + " " + box.getBoundingClientRect().top);
+        //     const currentScroll = window.pageYOffset;
+        //     if (currentScroll <= checkpoint) {
+        //         opacity = 1 - currentScroll / checkpoint;
+        //     } else {
+        //         opacity = 0;
+        //     }
 
-            box.style.opacity = opacity;
-        });
+        //     box.style.opacity = opacity;
+        // });
 
 
 
-        const box: any = this.findInsideMe(".box");
-        const TDBox: any = this.findInsideMe(".TDBox");
-        const clouds: any = this.findInsideMe(".cloud", true);
-        window.onscroll = (_: any) => {
-            console.log(`X: ${window.scrollX} Y: ${window.scrollY}`)
-            const p = (window.scrollY / window.innerHeight) * 100;
-            box.style.left = `${p}%`;
-            box.style.opacity = `${p / 100}`;
+        // const box: any = this.findInsideMe(".box");
+        // const TDBox: any = this.findInsideMe(".TDBox");
+        // const clouds: any = this.findInsideMe(".cloud", true);
+        // window.onscroll = (_: any) => {
+        //     console.log(`X: ${window.scrollX} Y: ${window.scrollY}`)
+        //     const p = (window.scrollY / window.innerHeight) * 100;
+        //     box.style.left = `${p}%`;
+        //     box.style.opacity = `${p / 100}`;
 
-            clouds[0].style.left = `${p * 2}%`;
-            clouds[1].style.left = `${p * 1.3}%`;
+        //     clouds[0].style.left = `${p * 2}%`;
+        //     clouds[1].style.left = `${p * 1.3}%`;
 
-            if (window.scrollY >= 340) {
-                TDBox.style.opacity = 1;
-                if (window.scrollY >= 350) {
-                    TDBox.style.transform = `rotateY(${window.scrollY}deg)`;
-                }
-            } else {
-                TDBox.style.opacity = 0;
-            }
-        }
+        //     if (window.scrollY >= 340) {
+        //         TDBox.style.opacity = 1;
+        //         if (window.scrollY >= 350) {
+        //             TDBox.style.transform = `rotateY(${window.scrollY}deg)`;
+        //         }
+        //     } else {
+        //         TDBox.style.opacity = 0;
+        //     }
+        // }
 
 
 
